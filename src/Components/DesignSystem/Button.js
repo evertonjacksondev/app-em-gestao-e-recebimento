faff11e45f8c3be199632ea10f49804aa188fa85
MacDantas <marcelodecarvalhodantas@hotmail.com>
2022-12-01 21:04:02 -0300

    Button style standard finished

--- src/Components/DesignSystem/Button.js
+++ src/Components/DesignSystem/Button.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import "material-icons/iconfont/material-icons.css";
 
-const Button = () => {
-  const Button = styled.button({ 
-    color: 'black',
-    borderRadius: '5px',
-    backgroundColor: 'yellow',
-    width: '100px',
-    height: '30px',
-   })
+const Button = (props) => {
+  const { name = "Salvar", background = "#587FA6", icon = "close" } = props;
+  const Button = styled.button`
+    background: ${background};
+    color: white;
+    border: 1px solid #587FA6;
+    border-radius: 7px;
+    padding: 20px;
+    width: 150px;
+    height: 20px;
+    font-size: 18px;
+    font-family: "Lato", sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    :hover {
+      box-shadow: 0 0 5px #808080;
+    }
+  `;
+
+  const Paragraph = styled.p`
+    margin: 5px;
+  `;
+
   return (
-    <Button>Button</Button>
-  )
-}
+    <Button>
+      <i class="material-icons">{icon}</i>
+      <Paragraph>{name}</Paragraph>
+    </Button>
+  );
+};
 
-export default Button
+export default Button;
