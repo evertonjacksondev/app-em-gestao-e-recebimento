fc2332a526f15857783d74fa4f9aeb3979b6d600
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-12-17 10:06:00 -0300

    add images carrol

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,6 +8,7 @@ import Image0 from './../../images/apartamento0.jpg'
 import Image1 from './../../images/apartamento1.jpg'
 import Image2 from './../../images/apartamento3.webp'
 import Image3 from './../../images/portaria.jpg'
+import Image4 from './../../images/gestao.jpg'
 import Table from '../../Components/DesignSystem/Combinations/Table'
 import styled from 'styled-components'
 import { Icon } from '../../Components/DesignSystem/Elements/Icon'
@@ -39,9 +40,16 @@ const Home = () => {
           autoPlay={true}
           infiniteLoop={true}
         >
+           <div>
+            <img src={Image4} />
+          </div>
+          <div>
+            <img src={Image4} />
+          </div>
           <div>
             <img src={Image0} />
           </div>
+         
           <div>
             <img src={Image1} />
           </div>
